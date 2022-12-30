072457d88e6ee6fad36d9f173d1c392cde72f689
Y0usefAli <121688049+Y0usefAli@users.noreply.github.com>
2022-12-30 19:45:39 +0200

    Add files via upload

---
+++ Project 1/Project 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inputs
+            Console.Write("Enter 1st number: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter 2nd number: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nThe prime numbers from {0} to {1}:", n1, n2);
+            //Check
+            for (int i = n1; i <= n2; i++)
+            {
+                bool prime = true;
+                for (int x = 2; x < i; x++)
+                {
+                    if (i % x == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Project 2/Project 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inputs
+            Console.Write("Enter the 1st number: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the 2nd number: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nThe perfect numbers from {0} to {1}:", n1, n2);
+            //Check
+            for (int i = n1; i <= n2; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
